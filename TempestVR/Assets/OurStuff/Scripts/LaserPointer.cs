0a17b6845f10b040181f98765f7a37003c97ac80
icecreamin <icecremein@gmail.com>
2019-02-18 00:36:00 -0800

    added steam vr camera rig, ability to grab objects, point a lazer at the ground and teleport using this tutorial: https://www.raywenderlich.com/9189-htc-vive-tutorial-for-unity

---
+++ TempestVR/Assets/OurStuff/Scripts/LaserPointer.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class LaserPointer : MonoBehaviour {
+
+	public SteamVR_Input_Sources handType;
+	public SteamVR_Behaviour_Pose controllerPose;
+	public SteamVR_Action_Boolean teleportAction;
+	
+	public GameObject laserPrefab; // 1
+	private GameObject laser; // 2
+	private Transform laserTransform; // 3
+	private Vector3 hitPoint; // 4
+	
+
+	public Transform cameraRigTransform; 
+	public GameObject teleportReticlePrefab;
+	private GameObject reticle;
+	private Transform teleportReticleTransform; 
+	public Transform headTransform; 
+	public Vector3 teleportReticleOffset; 
+	public LayerMask teleportMask; 
+	private bool shouldTeleport; 
+
+
+	
+	// Use this for initialization
+	void Start () {
+		// 1
+		laser = Instantiate(laserPrefab);
+		// 2
+		laserTransform = laser.transform;
+		
+		// 1
+		reticle = Instantiate(teleportReticlePrefab);
+		// 2
+		teleportReticleTransform = reticle.transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 1
+		if (teleportAction.GetState(handType))
+		{
+			RaycastHit hit;
+
+			// 2
+			if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, 100, teleportMask))
+			{
+				hitPoint = hit.point;
+				ShowLaser(hit);
+				// 1
+				reticle.SetActive(true);
+				// 2
+				teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+				// 3
+				shouldTeleport = true;
+
+			}
+		}
+		else // 3
+		{
+			laser.SetActive(false);
+			reticle.SetActive(false);
+		}
+		
+		if (teleportAction.GetStateUp(handType) && shouldTeleport)
+		{
+			Teleport();
+		}
+
+	}
+	
+	private void ShowLaser(RaycastHit hit)
+	{
+		// 1
+		laser.SetActive(true);
+		// 2
+		laserTransform.position = Vector3.Lerp(controllerPose.transform.position, hitPoint, .5f);
+		// 3
+		laserTransform.LookAt(hitPoint);
+		// 4
+		laserTransform.localScale = new Vector3(laserTransform.localScale.x,
+												laserTransform.localScale.y,
+												hit.distance);
+	}
+	
+	private void Teleport()
+	{
+		// 1
+		shouldTeleport = false;
+		// 2
+		reticle.SetActive(false);
+		// 3
+		Vector3 difference = cameraRigTransform.position - headTransform.position;
+		// 4
+		difference.y = 0;
+		// 5
+		cameraRigTransform.position = hitPoint + difference;
+	}
+
+}
