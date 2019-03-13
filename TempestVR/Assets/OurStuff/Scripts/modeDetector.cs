661207b44c3125c61601daf64d6714508e472b95
Wade King <wkingnumber2@gmail.com>
2019-03-13 02:00:58 -0700

    detector to sense if vr headset is connected and switch modes

---
+++ TempestVR/Assets/OurStuff/Scripts/modeDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class modeDetector : MonoBehaviour {
+    public GameObject vrPlayer;
+    public GameObject fpsPlayer;
+    private void Awake()
+    {
+        if (XRDevice.isPresent)
+        {
+            Debug.Log("VR device detected, loading VR mode");
+            vrPlayer.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("No VR device detected, loading Desktop mode");
+            fpsPlayer.SetActive(true);
+        }
+        
+    }
+    
+}
